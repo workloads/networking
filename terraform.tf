7e4a483bcf88f6473524ebb89b19babd17167736
Kerim Satirli <k.satirli@gmail.com>
2022-09-01 09:59:07 +0200

    bumps HCP provider from `0.41.0` to `0.42.0`

--- terraform.tf
+++ terraform.tf
@@ -18,10 +18,10 @@ terraform {
       version = "4.28.0"
     }
 
-    # see https://registry.terraform.io/providers/hashicorp/hcp/0.42.0/
+    # see https://registry.terraform.io/providers/hashicorp/hcp/0.43.0/
     hcp = {
       source  = "hashicorp/hcp"
-      version = ">= 0.42.0, < 1.0.0"
+      version = ">= 0.43.0, < 1.0.0"
     }
 
     # see https://registry.terraform.io/providers/hashicorp/tfe/0.36.1/
