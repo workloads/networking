177ca0e0c65e955c12d09dd681ab4d9ba63da525
Kerim Satirli <k.satirli@gmail.com>
2023-01-16 17:04:20 +0100

    adds default tags for AWS resources

--- providers.tf
+++ providers.tf
@@ -2,6 +2,14 @@
 # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#schema
 provider "aws" {
   region = var.management_region_aws
+
+  # see https://registry.terraform.io/providers/hashicorp/aws/latest/docs#default_tags
+  default_tags {
+    tags = {
+      "github:url"                   = "https://github.com/workloads/networking"
+      "terraform-cloud:organization" = var.tfe_organization
+    }
+  }
 }
 
 # The HCP Provider is set to retrieve configuration from the executing environment
