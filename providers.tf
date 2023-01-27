9947e1a88454ea6b719a7e7a93498da327f87381
Kerim Satirli <k.satirli@gmail.com>
2023-01-27 17:17:52 +0100

    sets TFE defaults to Terraform Cloud

--- providers.tf
+++ providers.tf
@@ -18,4 +18,7 @@ provider "hcp" {}
 
 # The Terraform Cloud Provider is set to retrieve configuration from the executing environment
 # see https://registry.terraform.io/providers/hashicorp/tfe/latest/docs#authentication
-provider "tfe" {}
+provider "tfe" {
+  hostname        = "app.terraform.io"
+  ssl_skip_verify = false
+}
