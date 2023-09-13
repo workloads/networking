a02932077dfb273261f8e299828be9aa759ae387
Kerim Satirli <k.satirli@gmail.com>
2023-09-13 10:57:27 +0200

    linting

--- terraform_cloud_variables.tf
+++ terraform_cloud_variables.tf
@@ -3,6 +3,6 @@ resource "tfe_variable" "hvn_id_aws" {
   key             = "hvn_id_aws"
   value           = module.hvn_aws.hcp_hvn.hvn_id
   category        = "terraform"
-  description     = "The ID of the HVN this HCP Vault Cluster is associated to."
+  description     = "The ID of the HashiCorp Virtual Network."
   variable_set_id = data.tfe_variable_set.hvn_ids.id
 }
