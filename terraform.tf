c955d02b183f54c620433f9b75771e74ba8b744e
Kerim Satirli <k.satirli@gmail.com>
2024-06-12 13:35:25 +0200

    linting

--- terraform.tf
+++ terraform.tf
@@ -34,5 +34,5 @@ terraform {
   }
 
   # see https://developer.hashicorp.com/terraform/language/settings#specifying-a-required-terraform-version
-  required_version = ">= 1.8.0, < 1.10.0"
+  required_version = ">= 1.8.0, < 2.0.0"
 }
